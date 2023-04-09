4272e9def2ce0def52a32a938efabc30134805fc
Henry Miheso <mwenesi@gmail.com>
2023-04-09 14:22:10 +0200

    1st commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import streamlit as st
+from io import StringIO
+from vector_search import *
+from utils import *
+import qa
+
+st.markdown('<h1 style="text-align: center">AskAbu</h1>', unsafe_allow_html=True)
+st.markdown('<h9 style="text-align: center">GTP Search Engine for Businesses</h9>', unsafe_allow_html=True)
+
+url = False
+index = ""
+query = False
+
+options = st.radio(
+    'Choose task',
+    ('Ask a question','Update your Database'))
+
+if 'Update the Database' in options:
+    url = st.text_input("Enter the url of the document")
+    
+if 'Ask a question' in options:
+    query = st.text_input("Enter your question")
+
+button = st.button("Submit")
+  
+if button and (url or query):
+    if 'Update the Database' in options:
+        with st.spinner("Updating Database..."):
+            corpusData = scrape_text_from_url(url)
+            addData(corpusData,url)
+            st.success("Database Updated")
+    if 'Ask a question' in options:
+        with st.spinner("Searching for the answer..."):
+            urls,res = find_match(query,2)
+            context= "\n\n".join(res)
+            st.expander("Context").write(context)
+            prompt = qa.create_prompt(context,query)
+            answer = qa.generate_answer(prompt)
+            st.success("Answer: "+answer)
+
+            
+            
+
+
+       
---
+++ qa.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import openai
+
+
+
+
+# Authenticate with the OpenAI API key
+openai.api_key = st.secrets["openai"]["api_key"]
+
+def create_prompt(context,query):
+    header = "You are an expert on East African countries. Answer the question and you can deviate from the provided context if neccessary, and if the answer is not contained within the text and requires some latest information to be updated, print 'Sorry Not Sufficient context to answer query' \n"
+    return header + context + "\n\n" + query + "\n"
+
+def generate_answer(prompt):
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=prompt,
+    temperature=0.8,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0,
+    stop = [' END']
+    )
+    return (response.choices[0].text).strip()
