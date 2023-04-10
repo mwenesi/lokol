2865a8790c6323deb13fb332aaa1ea9de9b0c900
Henry Miheso <mwenesi@gmail.com>
2023-04-10 15:44:59 +0200

    Edits & Cleanup

--- main.py
+++ main.py
@@ -1,45 +1,36 @@
 import streamlit as st
-from io import StringIO
 from vector_search import *
 from utils import *
 import qa
 
+st.sidebar.markdown('<h3>Add a website link</h3>', unsafe_allow_html=True)
+
+url = st.sidebar.text_input("Enter the url of the document")
+
+
+if st.sidebar.button("Update Database") and url:
+    with st.spinner("Updating Database..."):
+        corpusData = scrape_text_from_url(url)
+        addData(corpusData,url)
+        st.sidebar.success("Database Updated")
+
 st.markdown('<h1 style="text-align: center">AskAbu</h1>', unsafe_allow_html=True)
 st.markdown('<h9 style="text-align: center">GTP Search Engine for Businesses</h9>', unsafe_allow_html=True)
 
-url = False
-index = ""
-query = False
+st.write("")
+st.write("")
+st.write("")
 
-options = st.radio(
-    'Choose task',
-    ('Ask a question','Update your Database'))
+st.markdown('<h3 style="text-align: center">Ask a Question</h3>', unsafe_allow_html=True)
 
-if 'Update the Database' in options:
-    url = st.text_input("Enter the url of the document")
-    
-if 'Ask a question' in options:
-    query = st.text_input("Enter your question")
-
+query = st.text_input("Enter your question")
 button = st.button("Submit")
-  
-if button and (url or query):
-    if 'Update the Database' in options:
-        with st.spinner("Updating Database..."):
-            corpusData = scrape_text_from_url(url)
-            addData(corpusData,url)
-            st.success("Database Updated")
-    if 'Ask a question' in options:
-        with st.spinner("Searching for the answer..."):
-            urls,res = find_match(query,2)
-            context= "\n\n".join(res)
-            st.expander("Context").write(context)
-            prompt = qa.create_prompt(context,query)
-            answer = qa.generate_answer(prompt)
-            st.success("Answer: "+answer)
 
-            
-            
-
-
-       
+if button and query:
+    with st.spinner("Searching for the answer..."):
+        urls,res = find_match(query,2)
+        context= "\n\n".join(res)
+        st.expander("Context").write(context)
+        prompt = qa.create_prompt(context,query)
+        answer = qa.generate_answer(prompt)
+        st.success("Answer: "+answer)
--- vector_search.py
+++ vector_search.py
@@ -4,37 +4,16 @@ import streamlit as st
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
-pinecone_config = st.secrets["Pinecone"]
-api_key = pinecone_config["PINECONE_API_KEY"]
-environment = pinecone_config["PINECONE_ENVIRONMENT"]
-index_name = pinecone_config["PINECONE_INDEX_NAME"]
+secrets = st.secrets["pinecone"]
+api_key = secrets["api_key"]
+environment = secrets["environment"]
+index_name = secrets["index_name"]
 
 # Initialize Pinecone
 pinecone.init(api_key=api_key, environment=environment)
 
 # Get the index
 index = pinecone.Index(index_name)
-
-def addData(corpusData,url):
-    id = id = index.describe_index_stats()['total_vector_count']
-    for i in range(len(corpusData)):
-        chunk=corpusData[i]
-        chunkInfo=(str(id+i),
-                model.encode(chunk).tolist(),
-                {'title': url,'context': chunk})
-        index.upsert(vectors=[chunkInfo])
-
-def find_match(query,k):
-    query_em = model.encode(query).tolist()
-    result = index.query(query_em, top_k=k, includeMetadata=True)
-    
-    return [result['matches'][i]['metadata']['title'] for i in range(k)],[result['matches'][i]['metadata']['context'] for i in range(k)]
-import pinecone
-from sentence_transformers import SentenceTransformer,util
-model = SentenceTransformer('all-MiniLM-L6-v2')
-
-pinecone.init(api_key="f174c324-e266-4d94-9796-555feb361470", environment="us-west4-gcp") 
-index = pinecone.Index("tasks")
 
 def addData(corpusData,url):
     id = id = index.describe_index_stats()['total_vector_count']
